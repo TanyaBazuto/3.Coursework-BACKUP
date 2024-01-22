f90bbfd0cdaac450877c85b82bd9c7ee67e1e37c
Tatiana Bazuto <tbazuto@gmail.com>
2024-01-22 20:58:51 +0300

    22-11-2023

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 from tqdm import tqdm
 from time import sleep
 
-
 class PhotosLoad:
     def __init__(self, token, user_id):
         self.token = token
@@ -27,9 +26,9 @@ class PhotosLoad:
         profile_list = self.get_profile_photos()
         photo_count = profile_list['response']['count']
         i = 0
-        photos_load = []
-        max_size_photo = {}
+        max_size_photo = {}  # Словарь {Название фото - URL фото максимального разрешения}
 
+        # Создаём папку на компьютере для скачивания фотографий
         if not os.path.exists('vk_backup'):
             os.mkdir('vk_backup')
 
@@ -37,10 +36,12 @@ class PhotosLoad:
             if i != 0:
                 profile_list = self.get_profile_photos()
 
+            # Проход по фотографиям
             for photo in tqdm(profile_list['response']['items']):
                 sleep(0.25)
                 max_size = 0
                 photos_info = {}
+                # Выбор фото максимального разрешения и добавление в словарь {max_size_photo}
                 for size in photo['sizes']:
                     if size['height'] >= max_size:
                         max_size = size['height']
@@ -51,40 +52,48 @@ class PhotosLoad:
                     max_size_photo[f"{photo['likes']['count']} + {photo['date']}"] = size['url']
                     photos_info['file_name'] = f"{photo['likes']['count']} + {photo['date']}.jpg"
 
-                photos_info['size'] = size['type']
-                photos_load.append(photos_info)
+        # Cписок всех фотографий
+        photos_load = []  # Список загруженных фото
+        photos_info['size'] = size['type']
+        photos_load.append(photos_info)
 
-            for photo_name, photo_url in max_size_photo.items():
-                with open('vk_backup/%s' % f'{photo_name}.jpg', 'wb') as file:
-                    img = requests.get(photo_url)
-                    file.write(img.content)
+        # Скачиваем фотографии
+        for photo_name, photo_url in max_size_photo.items():
+            with open('vk_backup/%s' % f'{photo_name}.jpg', 'wb') as file:
+                img = requests.get(photo_url)
+                file.write(img.content)
 
-                with open('photos_load.json', 'w') as file:
-                    json.dump(photos_load, file)
+        # Запись информации о скачанных фоторафиях в файл .json
+        with open('photos_load.json', 'w') as file:
+            json.dump(photos_load, file)
 
 
-class PhotosUpload:
-    def __init__(self, token):
-        self.token = token
 
-    def ya_folder_create(self):
-        url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {'path': 'image_backup_vk'
-                  }
-        headers = {'Authorization': f'OAuth {ya_token}'
-                   }
-        response = requests.put(url=url_create_folder, headers=headers, params=params)
-        return response.json()
+# Создаем папку на Яндекс.Диске
+url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
+headers = {'Content-Type': 'application/json',
+           'Accept': 'application/json',
+           'Authorization': f'OAuth {token}'
+           }
+params = {'path': 'image_vk'}
+response = requests.put(url=url_create_folder, headers=headers, params=params)
 
-    def upload_vk_image(self):
-        url_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        headers = {'Authorization': f'OAuth {ya_token}'
-                   }
-        params = {'path': 'image_backup_vk/image'
-                  }
-        response = requests.get(url=url_upload, headers=headers, params=params)
-        link_upload = response.json().get('href')
+# Загружаем фото на Яндекс.Диск
+url_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+headers = {'Content-Type': 'application/json',
+           'Accept': 'application/json',
+           'Authorization': f'OAuth {token}'
+           }
+params = {'path': 'image_vk/{file_image}'}
 
-        with open('vk_backup/img', 'rb') as file:
-            response = requests.put(link_upload, files={'file': file})
-            return response.json()
+# Получение ссылки на загрузку
+response = requests.get(url=url_upload, headers=headers, params=params)
+link_upload = response.json().get('href')
+
+# Загрузка файла
+with open('vk_backup/{file_image}', 'rb') as file:
+    try:
+        requests.put(link_upload, files={'file': file})
+        print(response.json())
+    except KeyError:
+        print(response)
